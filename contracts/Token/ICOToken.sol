33a94965129f80cd68a983fdaa505ebe072af39e
Vladdy <ivanov.vladdy@gmail.com>
2018-05-14 14:07:15 +0300

    ICOToken extends PausableToken instead just Pausable

--- contracts/Token/ICOToken.sol
+++ contracts/Token/ICOToken.sol
@@ -10,16 +10,16 @@ import "zeppelin-solidity/contracts/lifecycle/Pausable.sol";
  * @dev Very simple ERC20 Token example.
  * `StandardToken` functions.
  */
-contract ICOToken is MintableToken, Pausable {
+contract ICOToken is MintableToken, PausableToken {
 
-  string public constant name = "ICO Token";
-  string public constant symbol = "ICO";
-  uint8 public constant decimals = 18;
+    string public constant name = "ICO Token";
+    string public constant symbol = "ICO";
+    uint8 public constant decimals = 18;
 
 
-  /**
-   * @dev Constructor that gives msg.sender all of existing tokens.
-   */
-  constructor() public {
-  }
+    /**
+     * @dev Constructor that gives msg.sender all of existing tokens.
+     */
+    constructor() public {
+    }
 }
